92ad0e82c8d6397be9bbfee3926bdb33bd062fb4
Zolko1995 <zoltanberkula@gmail.com>
2022-10-02 20:00:45 +0200

    initial commit

---
+++ portfolio-site/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+
+  content: [
+    "./pages/**/*.{js, ts, jsx, tsx}",
+    "./components/**/*.{js, ts, jsx, tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        burtons: "burtons",
+      },
+      colors: {
+        charCoal: "#2F3E46",
+        darkSlateGray: "#354F52",
+        hookersGreen: "#52796F",
+        darkSeaGreen: "#84A98C",
+        ashGray: "#CAD2C5",
+      },
+    },
+  },
+  plugins: [],
+};
